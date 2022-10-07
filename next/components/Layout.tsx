d8952f0749d4a8adbc7c7ae013d8adeee3c2a77c
Tushar S Agrawal <tusharsagrawal16@gmail.com>
2022-10-07 15:57:32 +0530

    Commit before making responsive

--- next/components/Layout.tsx
+++ next/components/Layout.tsx
@@ -1,24 +1,20 @@
 import React from "react";
-import Header from "./Header";
-
-import useMobileView from "../hooks/useMobileView";
-import MobileHeader from "./MobileHeader";
-
 import styles from "../styles/Layout.module.css";
 import useGreaterThanView from "../hooks/useGreaterThanView";
+import Header from "./Header";
+import Footer from "./Footer";
+import MobileHeader from "./MobileHeader";
 import LeftBar from "./LeftBar";
 import RightBar from "./RightBar";
-import Footer from "./Footer";
 
 const Layout = ({ children }: { children: JSX.Element }) => {
-  const isMobileView = useMobileView();
-  const isGreaterView = useGreaterThanView([576, 768, 992]);
+  const isGreaterView = useGreaterThanView([576, 768]);
 
   return (
     <>
-      {isMobileView ? <MobileHeader /> : <Header />}
+      {isGreaterView[0] ? <Header /> : <MobileHeader />}
       <div className={styles.pageContainer}>
-        {!isMobileView && <LeftBar />}
+        {isGreaterView[0] && <LeftBar />}
         <div className={styles.footerContainer}>
           <div className={styles.container}>{children}</div>
           <Footer />
--- next/components/LeftBar.tsx
+++ next/components/LeftBar.tsx
@@ -7,16 +7,16 @@ const iconVariants: Variants = {
     pathLength: 0,
     opacity: 0,
   },
-  visible: {
+  visible: (delay) => ({
     pathLength: 1,
     opacity: 1,
     transition: {
       opacity: {
         duration: 0.3,
       },
-      delay: 1.5,
+      delay: delay * 0.3 + 1.5,
     },
-  },
+  }),
 };
 
 const barVariant: Variants = {
@@ -33,6 +33,35 @@ const LeftBar = () => {
     <motion.div className={styles.leftSide} variants={barVariant} initial="hidden" animate="visible">
       <div className={styles.fixedContainer}>
         <div className={styles.iconsContainer}>
+          <a href="mailto:tusharsagrawal16@gmail.com">
+            <motion.svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="20"
+              height="20"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="var(--secondary)"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              whileHover={{ scale: 1.2, y: -5 }}
+            >
+              <motion.path
+                initial="hidden"
+                animate="visible"
+                variants={iconVariants}
+                custom={0}
+                d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"
+              />
+              <motion.polyline
+                custom={0.2}
+                initial="hidden"
+                animate="visible"
+                variants={iconVariants}
+                points="22,6 12,13 2,6"
+              />
+            </motion.svg>
+          </a>
           <a>
             <motion.svg
               xmlns="http://www.w3.org/2000/svg"
@@ -49,6 +78,7 @@ const LeftBar = () => {
               <motion.path
                 initial="hidden"
                 animate="visible"
+                custom={1}
                 variants={iconVariants}
                 d="M13 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V9z"
               />
@@ -68,6 +98,7 @@ const LeftBar = () => {
               <motion.path
                 initial="hidden"
                 animate="visible"
+                custom={2}
                 variants={iconVariants}
                 d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"
               />
@@ -89,6 +120,7 @@ const LeftBar = () => {
               <motion.path
                 initial="hidden"
                 animate="visible"
+                custom={3}
                 variants={iconVariants}
                 d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"
               />
@@ -97,11 +129,20 @@ const LeftBar = () => {
                 y="9"
                 width="4"
                 height="12"
+                custom={3.2}
                 initial="hidden"
                 animate="visible"
                 variants={iconVariants}
               />
-              <motion.circle cx="4" cy="4" r="2" initial="hidden" animate="visible" variants={iconVariants} />
+              <motion.circle
+                custom={3.4}
+                cx="4"
+                cy="4"
+                r="2"
+                initial="hidden"
+                animate="visible"
+                variants={iconVariants}
+              />
             </motion.svg>
           </a>
           <a href="https://twitter.com/tushar_yaar" target="_blank" rel="noreferrer">
@@ -118,6 +159,7 @@ const LeftBar = () => {
               whileHover={{ scale: 1.2, y: -5 }}
             >
               <motion.path
+                custom={4}
                 variants={iconVariants}
                 initial="hidden"
                 animate="visible"
@@ -126,7 +168,7 @@ const LeftBar = () => {
             </motion.svg>
           </a>
         </div>
-        <span>SOCIALS</span>
+        <span>LINKS</span>
       </div>
     </motion.div>
   );
