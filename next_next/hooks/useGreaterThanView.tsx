6df98b63868a9cf7e636b6c29626fdf414769a87
Tushar S Agrawal <tusharsagrawal16@gmail.com>
2026-04-05 23:42:12 +0530

    migrate to next 16

---
+++ next_next/hooks/useGreaterThanView.tsx
@@ -0,0 +1,23 @@
+import { useState, useEffect } from "react";
+
+const checkWidth = (width: number, values: number[]) => {
+  return values.map((value) => width > value);
+};
+
+const useGreaterThanView = (width: number[]) => {
+  const [state, setState] = useState<boolean[]>(
+    typeof window === "undefined"
+      ? new Array(width.length).fill(true, 0, width.length)
+      : checkWidth(window.innerWidth, width)
+  );
+
+  useEffect(() => {
+    const handleResize = () => setState(checkWidth(window.innerWidth, width));
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, [width, state]);
+
+  return state;
+};
+
+export default useGreaterThanView;
